4ebd0b9b28acc1edca0779d16f9d6d3bbc9967cf
Arif Narayana <alfiannaila.517@gmail.com>
2026-09-22 08:15:27 +0700

    Sunsea admin: fix 'Buka Website' link to point to new website-sunsea template (was 404-ing at BASE_URL/home.php)

--- modules/sunsea/layout-header.php
+++ modules/sunsea/layout-header.php
@@ -1219,7 +1219,7 @@ if (empty($sunseaNavItemsVisible)) {
         </nav>
 
         <div class="ss-sidebar-footer">
-            <a href="<?php echo BASE_URL; ?>/home.php" target="_blank" rel="noopener" class="ss-website-btn">
+            <a href="<?php echo BASE_URL; ?>/website-sunsea/public/home.php" target="_blank" rel="noopener" class="ss-website-btn">
                 <i data-feather="external-link"></i> Buka Website
             </a>
             <div class="ss-user-block">
